4973372d68849d757896d4dde5b919f35b01c0c8
terry-for-github <anzi20000525@163.com>
2024-08-15 14:56:36 +0800

    update arguments

--- arguments/data_arguments.py
+++ arguments/data_arguments.py
@@ -4,25 +4,44 @@ from typing import Literal, Optional
 
 @dataclass(frozen=True)
 class DataArguments:
-    data_path: str = field(metadata={"help": "Path to json data file"})
+    json_path: str = field(metadata={"help": "Path to json data file"})
     image_folder: Optional[str] = field(default=None, metadata={
         "help": "Path to image folder"
     })
 
-    lazy_preprocess: bool = field(default=False, metadata={
-        "help": "Whether to lazy preprocess data"
-    })
-    # must multimodal
-    # is_multimodal: bool = field(default=False, metadata={
-    # "help": "Whether the data is multimodal"})
     image_process_mode: Literal['pad', 'resize', 'crop'] = field(default='pad', metadata={
         "help": "Mode to process image\n"
                 "- pad: pad the image to square\n"
                 "- resize: resize the image to square\n"
                 "- crop: crop the image to square\n"
     })
+
     image_mark: str = field(default="<image>", metadata={
         "help": "The mark for image token in conversation"
+    })
+
+    dialog_key: str = field(default="conversations", metadata={
+        "help": "Key for dialog"
+    })
+
+    image_key: str = field(default="image", metadata={
+        "help": "Key for image"
+    })
+
+    role_key: str = field(default="from", metadata={
+        "help": "Key for role"
+    })
+
+    content_key: str = field(default="value", metadata={
+        "help": "Key for content"
+    })
+
+    human_key: str = field(default="human", metadata={
+        "help": "Key for human"
+    })
+
+    gpt_key: str = field(default="gpt", metadata={
+        "help": "Key for gpt"
     })
 
     def __str__(self):
