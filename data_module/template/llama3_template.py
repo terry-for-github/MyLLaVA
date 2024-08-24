1c8e154fadd07811cc9d1112acbcc67ccb7166fb
terry-for-github <anzi20000525@163.com>
2024-08-25 01:04:49 +0800

    update: llama3 template

--- data_module/template/llama3_template.py
+++ data_module/template/llama3_template.py
@@ -5,6 +5,7 @@ from .base_template import BaseTemplate
 
 @dataclass(frozen=True)
 class Llama3Template(BaseTemplate):
+    sys_role: str = field(default="system")
     human_role: str = field(default="user")
     gpt_role: str = field(default="assistant")
     system_prompt: str = field(default=(
@@ -20,15 +21,42 @@ class Llama3Template(BaseTemplate):
             f"{{% set loop_messages = messages %}}"
             f"{{% for message in loop_messages %}}"
             f"{{% if loop.index0 == 0 %}}"
-            f"{{{{ bos_token + '{self.system_prompt}\n' }}}}"
+            f"{{{{ bos_token }}}}"
+            f"{{{{ '<|start_header_id|>' + '{self.sys_role}' + '<|end_header_id|>\n\n' }}}}"
+            f"{{{{ '{self.system_prompt}' + eos_token }}}}"
             f"{{% endif %}}"
             f"{{% if loop.index0 % 2 == 0 %}}"
             f"{{{{ '<|start_header_id|>' + '{self.human_role}' + '<|end_header_id|>\n\n' }}}}"
-            f"{{{{ message['content'] | trim + '<|eot_id|>' }}}}"
+            f"{{{{ message['content']|trim + eos_token }}}}"
             f"{{% else %}}"
             f"{{{{ '<|start_header_id|>' + '{self.gpt_role}' + '<|end_header_id|>\n\n' }}}}"
             f"{{% generation %}}"
-            f"{{{{ message['content'] | trim + '<|eot_id|>' }}}}"
+            f"{{{{ message['content']|trim + eos_token }}}}"
+            f"{{% endgeneration %}}"
+            f"{{% endif %}}"
+            f"{{% endfor %}}"
+            f"{{% if add_generation_prompt %}}"
+            f"{{{{ '<|start_header_id|>' + '{self.gpt_role}' + '<|end_header_id|>\n\n'}}}}"
+            f"{{% endif %}}"
+        )
+
+
+@dataclass(frozen=True)
+class Llama3PlainTemplate(Llama3Template):
+    def get_template(self):
+        return (
+            f"{{% set loop_messages = messages %}}"
+            f"{{% for message in loop_messages %}}"
+            f"{{% if loop.index0 == 0 %}}"
+            f"{{{{ bos_token }}}}"
+            f"{{% endif %}}"
+            f"{{% if loop.index0 % 2 == 0 %}}"
+            f"{{{{ '<|start_header_id|>' + '{self.human_role}' + '<|end_header_id|>\n\n' }}}}"
+            f"{{{{ message['content']|trim + eos_token }}}}"
+            f"{{% else %}}"
+            f"{{{{ '<|start_header_id|>' + '{self.gpt_role}' + '<|end_header_id|>\n\n' }}}}"
+            f"{{% generation %}}"
+            f"{{{{ message['content']|trim + eos_token }}}}"
             f"{{% endgeneration %}}"
             f"{{% endif %}}"
             f"{{% endfor %}}"
