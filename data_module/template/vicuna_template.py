55097634f979485411c543bc5fc795c0a57d750a
terry-for-github <anzi20000525@163.com>
2024-08-25 17:16:37 +0800

    update: vicuna template

--- data_module/template/vicuna_template.py
+++ data_module/template/vicuna_template.py
@@ -5,8 +5,8 @@ from .base_template import BaseTemplate
 
 @dataclass(frozen=True)
 class VicunaTemplate(BaseTemplate):
-    human_role: str = field(default="USER: ")
-    gpt_role: str = field(default="ASSISTANT: ")
+    human_role: str = field(default="USER")
+    gpt_role: str = field(default="ASSISTANT")
     system_prompt: str = field(default=(
         "A chat between a curious user and an artificial intelligence assistant. "
         "The assistant gives helpful, detailed, and polite answers to the user\\'s questions."
@@ -20,9 +20,9 @@ class VicunaTemplate(BaseTemplate):
             f"{{{{ bos_token + '{self.system_prompt}' }}}}"
             f"{{% endif %}}"
             f"{{% if loop.index0 % 2 == 0 %}}"
-            f"{{{{ '\n\n{self.human_role}' + message['content']|trim }}}}"
+            f"{{{{ '\n\n{self.human_role}: ' + message['content']|trim }}}}"
             f"{{% else %}}"
-            f"{{{{ '\n\n{self.gpt_role}' }}}}"
+            f"{{{{ '\n\n{self.gpt_role}: ' }}}}"
             f"{{% generation %}}"
             f"{{{{ message['content']|trim + eos_token }}}}"
             f"{{% endgeneration %}}"
