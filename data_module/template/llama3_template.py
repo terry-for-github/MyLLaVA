5ebd1bbd01245f353ea57f5783e6a3349fd02836
terry-for-github <anzi20000525@163.com>
2024-08-25 17:16:26 +0800

    remove: llama3 plain template

--- data_module/template/llama3_template.py
+++ data_module/template/llama3_template.py
@@ -39,28 +39,3 @@ class Llama3Template(BaseTemplate):
             f"{{{{ '<|start_header_id|>' + '{self.gpt_role}' + '<|end_header_id|>\n\n'}}}}"
             f"{{% endif %}}"
         )
-
-
-@dataclass(frozen=True)
-class Llama3PlainTemplate(Llama3Template):
-    def get_template(self):
-        return (
-            f"{{% set loop_messages = messages %}}"
-            f"{{% for message in loop_messages %}}"
-            f"{{% if loop.index0 == 0 %}}"
-            f"{{{{ bos_token }}}}"
-            f"{{% endif %}}"
-            f"{{% if loop.index0 % 2 == 0 %}}"
-            f"{{{{ '<|start_header_id|>' + '{self.human_role}' + '<|end_header_id|>\n\n' }}}}"
-            f"{{{{ message['content']|trim + eos_token }}}}"
-            f"{{% else %}}"
-            f"{{{{ '<|start_header_id|>' + '{self.gpt_role}' + '<|end_header_id|>\n\n' }}}}"
-            f"{{% generation %}}"
-            f"{{{{ message['content']|trim + eos_token }}}}"
-            f"{{% endgeneration %}}"
-            f"{{% endif %}}"
-            f"{{% endfor %}}"
-            f"{{% if add_generation_prompt %}}"
-            f"{{{{ '<|start_header_id|>' + '{self.gpt_role}' + '<|end_header_id|>\n\n'}}}}"
-            f"{{% endif %}}"
-        )
